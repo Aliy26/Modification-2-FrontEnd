d594b7b8960a3f42bc3899d512d152818b46cb45
Umar_Aliy <umaraliy092@gmail.com>
2024-10-18 20:29:40 +0900

    fix: fixed some of the problems across the project

--- libs/components/layout/LayoutBasic.tsx
+++ libs/components/layout/LayoutBasic.tsx
@@ -31,7 +31,7 @@ const withLayoutBasic = (Component: any) => {
         case "/property":
           title = "Property Search";
           desc = "We are glad to see you again!";
-          bgImage = "/img/banner/properties.png";
+          bgImage = "/img/banner/brand-logo.jpg";
           break;
         case "/agent":
           title = "Agents";
@@ -93,8 +93,8 @@ const withLayoutBasic = (Component: any) => {
       return (
         <>
           <Head>
-            <title>Nestar</title>
-            <meta name={"title"} content={`Nestar`} />
+            <title>Cozy Home</title>
+            <meta name={"title"} content={`Cozy Home`} />
           </Head>
           <Stack id="mobile-wrap">
             <Stack id={"top"}>
@@ -115,8 +115,8 @@ const withLayoutBasic = (Component: any) => {
       return (
         <>
           <Head>
-            <title>Nestar</title>
-            <meta name={"title"} content={`Nestar`} />
+            <title>Cozy Home</title>
+            <meta name={"title"} content={`Cozy Home`} />
           </Head>
           <Stack id="pc-wrap">
             <Stack id={"top"}>
