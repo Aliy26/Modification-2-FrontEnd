020afcf3384287efd70e561712f32c64032de182
Umar_Aliy <umaraliy092@gmail.com>
2025-04-16 18:51:57 +0900

    feat: developed a header swiper for newly added products

---
+++ libs/components/homepage/HeaderSwiperCard.tsx
@@ -0,0 +1,164 @@
+import React from "react";
+import { Stack, Box, Divider, Typography } from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import useDeviceDetect from "../../hooks/useDeviceDetect";
+import { Product } from "../../types/product/product";
+import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
+import { REACT_APP_API_URL, topProductRank } from "../../config";
+import { useRouter } from "next/router";
+import { useReactiveVar } from "@apollo/client";
+import { userVar } from "../../../apollo/store";
+import moment from "moment";
+
+interface HeaderSwiperCard {
+  product: Product;
+}
+
+const HeaderSwiperCard = (props: HeaderSwiperCard) => {
+  const { product } = props;
+  const device = useDeviceDetect();
+  const router = useRouter();
+  const user = useReactiveVar(userVar);
+
+  /** HANDLERS **/
+
+  const pushDetailHandler = async (productId: string) => {
+    console.log("ID:", productId);
+    await router.push({
+      pathname: "/product/detail",
+      query: { id: productId },
+    });
+  };
+
+  const pushAgentPage = async (agentId: string) => {
+    await router.push({
+      pathname: `/agent/detail`,
+      query: { agentId: agentId },
+    });
+  };
+
+  if (device === "mobile") {
+    return (
+      <Stack className="popular-card-box">
+        <Box
+          component={"div"}
+          className={"card-img"}
+          style={{
+            backgroundImage: `url(${REACT_APP_API_URL}/${product?.productImages[0]})`,
+          }}
+          onClick={() => {
+            pushDetailHandler(product._id);
+          }}
+        >
+          {product?.productRank && product?.productRank >= topProductRank ? (
+            <div className={"status"}>
+              <img src="/img/icons/electricity.svg" alt="" />
+              <span>top</span>
+            </div>
+          ) : (
+            ""
+          )}
+
+          <div className={"price"}>${product.productPrice}</div>
+        </Box>
+        <Box component={"div"} className={"info"}>
+          <strong
+            className={"title"}
+            onClick={() => {
+              pushDetailHandler(product._id);
+            }}
+          >
+            {product.productName}
+          </strong>
+
+          <div className={"options"}>
+            <div>
+              <img src="/img/icons/bed.svg" alt="" />
+            </div>
+            <div>
+              <img src="/img/icons/room.svg" alt="" />
+            </div>
+            <div>
+              <img src="/img/icons/expand.svg" alt="" />
+            </div>
+          </div>
+          <Divider sx={{ mt: "15px", mb: "17px" }} />
+          <div className={"bott"}>
+            <p>{product?.productRent ? "rent" : "sale"}</p>
+            <div className="view-like-box">
+              <IconButton color={"default"}>
+                <RemoveRedEyeIcon />
+              </IconButton>
+              <Typography className="view-cnt">
+                {product?.productViews}
+              </Typography>
+            </div>
+          </div>
+        </Box>
+      </Stack>
+    );
+  } else {
+    return (
+      <Stack className="popular-card-box">
+        <Box
+          component={"div"}
+          className={"card-img"}
+          style={{
+            backgroundImage: `url(${REACT_APP_API_URL}/${product?.productImages[0]})`,
+          }}
+          onClick={() => {
+            pushDetailHandler(product._id);
+          }}
+        >
+          {product?.productRank && product?.productRank >= topProductRank ? (
+            <div className={"status"}>
+              <img src="/img/icons/electricity.svg" alt="" />
+              <span>top</span>
+            </div>
+          ) : (
+            ""
+          )}
+
+          <div className={"price"}>${product.productPrice}</div>
+        </Box>
+        <Box component={"div"} className={"info"}>
+          <div className="name-date">
+            <strong
+              className={"title"}
+              onClick={() => {
+                pushDetailHandler(product._id);
+              }}
+            >
+              {product.productName}
+            </strong>
+            <span>
+              added{" "}
+              {moment().diff(product?.createdAt, "days") === 0
+                ? "today"
+                : moment().diff(product?.createdAt, "days") === 1
+                ? "yesterday"
+                : `${moment().diff(product?.createdAt, "days")} days ago`}
+            </span>
+          </div>
+
+          <Divider sx={{ mt: "10px", mb: "17px" }} />
+          <div className={"bott"}>
+            <p>
+              <span>{product?.productDesc}</span>
+            </p>
+          </div>
+          <div className="view-like-box">
+            <Typography
+              className="view-cnt"
+              onClick={() => pushAgentPage(product?.memberData?._id as string)}
+            >
+              by {product?.memberData?.memberNick}
+            </Typography>
+          </div>
+        </Box>
+      </Stack>
+    );
+  }
+};
+
+export default HeaderSwiperCard;
--- libs/components/layout/LayoutHome.tsx
+++ libs/components/layout/LayoutHome.tsx
@@ -1,32 +1,60 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import useDeviceDetect from "../../hooks/useDeviceDetect";
 import Head from "next/head";
 import Top from "../Top";
 import Footer from "../Footer";
 import { Stack } from "@mui/material";
-import FiberContainer from "../common/FiberContainer";
 import HeaderFilter from "../homepage/HeaderFilter";
 import { userVar } from "../../../apollo/store";
-import { useReactiveVar } from "@apollo/client";
+import { useQuery, useReactiveVar } from "@apollo/client";
 import { getJwtToken, updateUserInfo } from "../../auth";
 import Chat from "../Chat";
+import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 
 import { useRouter } from "next/router";
+import { Autoplay } from "swiper";
+import { Product } from "../../types/product/product";
+import { GET_PRODUCTS } from "../../../apollo/user/query";
+import { T } from "../../types/common";
+import HeaderSwiperCard from "../homepage/HeaderSwiperCard";
 
 const withLayoutMain = (Component: any) => {
   return (props: any) => {
+    const [popularProducts, setPopularProducts] = useState<Product[]>([]);
     const device = useDeviceDetect();
     const user = useReactiveVar(userVar);
     const router = useRouter();
-
+    const initialInput = {
+      page: 1,
+      limit: 15,
+      sort: "createdAt",
+      direction: "DESC",
+      search: {
+        productDesc: true,
+      },
+    };
     /** LIFECYCLES **/
     useEffect(() => {
       const jwt = getJwtToken();
       if (jwt) updateUserInfo(jwt);
     }, []);
+
+    const {
+      loading: getProductsLoading,
+      data: getProductsData,
+      error: getProductsError,
+      refetch: getProductsRefetch,
+    } = useQuery(GET_PRODUCTS, {
+      fetchPolicy: "cache-and-network",
+      variables: { input: initialInput },
+      notifyOnNetworkStatusChange: true,
+      onCompleted: (data: T) => {
+        setPopularProducts(data?.getProducts?.list);
+      },
+    });
 
     /** HANDLERS **/
 
@@ -71,13 +99,36 @@ const withLayoutMain = (Component: any) => {
             <Stack className={"header-main"}>
               {/* <FiberContainer />  */}
 
+              <div className="header-left">
+                <Swiper
+                  className={"left-swiper"}
+                  slidesPerView={"auto"}
+                  centeredSlides={true}
+                  spaceBetween={25}
+                  modules={[Autoplay]}
+                  autoplay={{
+                    delay: 2500,
+                    disableOnInteraction: false,
+                  }}
+                >
+                  {popularProducts.map((product: Product) => {
+                    return (
+                      <SwiperSlide
+                        key={product._id}
+                        className={"popular-property-slide"}
+                      >
+                        <HeaderSwiperCard product={product} />
+                      </SwiperSlide>
+                    );
+                  })}
+                </Swiper>
+              </div>
               <div className="promo" onClick={handleProductPage}>
                 <p>
                   Elevate Your Space! Shop Stylish Furniture & Essential
                   Appliances Now!
                 </p>
               </div>
-
               <Stack className={"container"}>{/* <HeaderFilter /> */}</Stack>
             </Stack>
             <div className="filter-container">
